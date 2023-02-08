b35b7f430c2f723c7feb32284e9c02fb217f6e7a
Joyce Shu <shuwei1203@gmail.com>
2023-02-08 13:21:39 +0000

    set up swagger

--- moonrakerbackend/moonrakerbackend/urls.py
+++ moonrakerbackend/moonrakerbackend/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_swagger.views import get_swagger_view
+from rest_framework.schemas import get_schema_view
+from django.views.generic import TemplateView
+
+schema_view = get_swagger_view(title='API Schema')
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include('api.urls')),
+    # path("api_schema/", schema_view),
+    path("api_schema/", get_schema_view(title='API Schema', description='Guide for the REST API')),
+    path("swagger_ui/", TemplateView.as_view(template_name='docs.html', extra_context={'schema_url':'api_schema'}), name='swagger-ui')
 ]
